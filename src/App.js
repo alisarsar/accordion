227fefad9dc631c522c35c4848126c82d2777416
Ali Sarsar <sarsralii@gmail.com>
2023-07-28 13:45:06 +0300

    accordion projesi yapıldı.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     return (
   <main>
     <div className='container'>
-      <h3>questions and answers about login</h3>
+      <h3> questions and answers about login</h3>
     <section className='info'>
       
        {questions.map((question)=> {
